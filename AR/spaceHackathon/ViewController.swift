079f4783abad82b66bc9742cc56d312ced39466b
Orsi Lukacs <ol14474@my.bristol.ac.uk>
2018-02-28 12:44:20 +0000

    Moved to AR folder

--- AR/spaceHackathon/ViewController.swift
+++ AR/spaceHackathon/ViewController.swift
@@ -9,13 +9,54 @@
 import UIKit
 import SceneKit
 import ARKit
+import Vision
+import AVFoundation
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate, AVCaptureVideoDataOutputSampleBufferDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    @IBOutlet private weak var cameraView: UIView? // The whole scene is a view
+    
+    @IBOutlet private weak var highlightView: UIView? { // Highlighted red cube for now -> Has to be replaced with car
+        didSet {
+            self.highlightView?.layer.borderColor = UIColor.red.cgColor
+            self.highlightView?.layer.borderWidth = 4
+            self.highlightView?.backgroundColor = .clear
+        }
+    }
+    
+    private let visionSequenceHandler = VNSequenceRequestHandler()
+    private lazy var cameraLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+    
+    // capture camera feed
+    private lazy var captureSession: AVCaptureSession = {
+        let session = AVCaptureSession()
+        session.sessionPreset = AVCaptureSession.Preset.photo
+        guard
+            let backCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back),
+            let input = try? AVCaptureDeviceInput(device: backCamera)
+            else { return session }
+        session.addInput(input)
+        return session
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // hide the red focus area on load
+        self.highlightView?.frame = .zero
+        
+        // make the camera appear on the screen
+        self.cameraView?.layer.addSublayer(self.cameraLayer)
+        
+        // register to receive buffers from the camera
+        let videoOutput = AVCaptureVideoDataOutput()
+        videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "MyQueue"))
+        self.captureSession.addOutput(videoOutput)
+        
+        // begin the session
+        self.captureSession.startRunning()
         
         // Set the view's delegate
         sceneView.delegate = self
@@ -25,7 +66,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
 //        // Create a new scene
 //        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-//        
+//
 //        // Set the scene to the view
 //        sceneView.scene = scene
     }
