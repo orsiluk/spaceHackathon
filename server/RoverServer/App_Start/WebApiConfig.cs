0a58b9d9d6c365ed475b8c7c7f1502e8464dacdc
Ross Gardiner <ross-public@gmx.com>
2018-02-28 16:44:59 +0000

    Make it possible to GET and POST commands

--- server/RoverServer/App_Start/WebApiConfig.cs
+++ server/RoverServer/App_Start/WebApiConfig.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using RoverServer.Controllers;
 
 namespace RoverServer
 {
@@ -16,9 +18,12 @@ namespace RoverServer
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Properties.TryAdd("CommandQueue", new ConcurrentQueue<Command>());
+            config.Properties.TryAdd("CommandList", new List<Command>());
         }
     }
 }
---
+++ server/RoverServer/Controllers/CommandsController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace RoverServer.Controllers
+{
+    public enum CommandType
+    {
+        Forward,
+        Backward
+    }
+
+    public class Command
+    {
+        [JsonProperty(Required = Required.Always)]
+        public int Id { get; set; }
+
+        [JsonProperty(Required = Required.Always)]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public CommandType CommandType { get; set; }
+
+        [JsonProperty(Required = Required.Always)]
+        public string Data { get; set; }
+
+        public override string ToString()
+        {
+            return $"Command {Id} : {CommandType} | {Data}";
+        }
+    }
+
+    public class CommandsController : ApiController
+    {
+        private List<Command> GetCommands()
+        {
+            object commands;
+            GlobalConfiguration.Configuration.Properties.TryGetValue("CommandList", out commands);
+            return (List<Command>) commands;
+        }
+
+        private bool IssueCommand(Command command)
+        {
+            GlobalConfiguration.Configuration.Properties.TryGetValue("CommandList", out object commandListObj);
+            var commandList = (List<Command>) commandListObj;
+            GlobalConfiguration.Configuration.Properties.TryGetValue("CommandQueue", out object commandQueueObj);
+            var commandQueue = (List<Command>) commandQueueObj;
+
+            if (commandList.Exists(cmd => command.Id == cmd.Id))
+            {
+                return false;
+            }
+
+            commandQueue.Add(command);
+            commandList.Add(command);
+            return true;
+        }
+
+        // GET api/commands/getall
+        public string GetAll()
+        {
+            return JsonConvert.SerializeObject(GetCommands());
+        }
+
+        // GET api/commands/5
+        public string Get(int id)
+        {
+            return JsonConvert.SerializeObject(GetCommands().Where(cmd => cmd.Id == id));
+        }
+
+        // POST api/commands/issue
+        [HttpPost]
+        [ActionName("Issue")]
+        public IHttpActionResult Issue([FromBody] Command value)
+        {
+            return IssueCommand(value)
+                ? Content(HttpStatusCode.OK, $"Successfully issue command {value.Id}")
+                : Content(HttpStatusCode.BadRequest, "Failed to issue command");
+        }
+    }
+}
