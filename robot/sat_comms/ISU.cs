eba1196d60e09fddfbd2b915cf0a503d648a3a8f
Edward Nutting <git@flingos.co.uk>
2018-02-28 15:48:32 +0000

    Trying to get ISU to work - C# serial is just plain wrong - works from Putty though.

---
+++ robot/sat_comms/ISU.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO.Ports;
+
+namespace sat_comms
+{
+    class ISU
+    {
+        private SerialPort port;
+
+        public ISU(string COMPort)
+        {
+            port = new SerialPort(COMPort, 19200, Parity.None, 8, StopBits.One);
+            port.Handshake = Handshake.RequestToSend;
+            port.NewLine = "\n";
+            port.Encoding = Encoding.ASCII;
+            port.DiscardNull = false;
+            port.ReadTimeout = 1000;
+        }
+
+        public void Open()
+        {
+            if (!port.IsOpen)
+            {
+                port.Open();
+                readResponse();
+            }
+        }
+
+        public void Close()
+        {
+            if (port.IsOpen)
+            {
+                port.Close();
+            }
+        }
+
+        private void issueExtendedATCommand(string cmd, string prefix = "")
+        {
+            string cmdString = "AT" + (prefix != "" ? "+" + prefix : "") + cmd;
+            Console.WriteLine("Issuing command: " + cmdString);
+            port.WriteLine(cmdString);
+        }
+
+        private Tuple<List<string>, bool> readResponse()
+        {
+            string line = "";
+            while (!line.EndsWith("OK"))
+            {
+                try
+                {
+                    int i = port.ReadByte();
+                    char c = (char)i;
+                    Console.Write(c);
+                    line += c;
+                }
+                catch(TimeoutException)
+                {
+                    // Do nothing
+                    // TODO
+                }
+            }
+            Console.Write('\n');
+            List<string> lines = line.Split("\n".ToCharArray()).ToList();
+            return new Tuple<List<string>, bool>(lines, lines[lines.Count-1].StartsWith("OK"));
+        }
+
+        public string ISN
+        {
+            get
+            {
+                issueExtendedATCommand("SN", "G");
+                Tuple<List<string>, bool> response = readResponse();
+                if (response.Item2)
+                {
+                    throw new Exception("Error reading ISN (IMEI) number. Data:\n" + String.Join("\n", response.Item1));
+                }
+                else if (response.Item1.Count != 3)
+                {
+                    throw new Exception("Error reading ISN (IMEI) number. Incorrect number of data items. Data:\n" + String.Join("\n", response.Item1));
+                }
+                else
+                {
+                    return response.Item1[0];
+                }
+            }
+        }
+    }
+}
--- robot/sat_comms/Program.cs
+++ robot/sat_comms/Program.cs
@@ -10,7 +10,40 @@ namespace sat_comms
     {
         static void Main(string[] args)
         {
+            ISU isu = null;
 
+            try
+            {
+                if (args.Length < 1)
+                {
+                    throw new Exception("Incorrect number of command line args. Args: COMPort");
+                }
+
+                isu = new ISU(args[0]);
+                isu.Open();
+
+                Console.WriteLine("Attempting to read ISN...");
+                string ISN = isu.ISN;
+                Console.WriteLine("Done. ISN is: " + ISN);
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (isu != null)
+                {
+                    isu.Close();
+                    isu = null;
+                }
+
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+
+                Console.WriteLine("Application will now exit.");
+            }
         }
     }
 }
