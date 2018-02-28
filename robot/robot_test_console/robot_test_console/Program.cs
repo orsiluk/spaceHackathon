51889c545a5392d9cd7c10f002d9f2e513f56aa2
Edward Nutting <git@flingos.co.uk>
2018-02-28 13:59:50 +0000

    Robot test demo working - requires NXT 2.0 Fantom driver

--- robot/robot_test_console/robot_test_console/Program.cs
+++ robot/robot_test_console/robot_test_console/Program.cs
@@ -4,12 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using NKH.MindSqualls;
+
 namespace robot_test_console
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Create a NXT brick,
+            // and use USB to communicate with it.
+            NxtBrick brick = new NxtBrick(NxtCommLinkType.USB, 0);
+
+            // Attach motors to port B and C on the NXT.
+            brick.MotorB = new NxtMotor();
+            brick.MotorC = new NxtMotor();
+            
+            // Syncronize the two motors.
+            NxtMotorSync motorPair = new NxtMotorSync(brick.MotorB, brick.MotorC);
+
+            brick.Connect();
+
+            // Query the device info, and write out the NXT's name.
+            Console.WriteLine("NXT name: {0}", brick.Name);
+
+            Console.WriteLine("Running motors B and C...");
+
+            // Run them at 75% power, for a 3600 degree run.
+            motorPair.Run(75, 3600, 0);
+            Console.WriteLine("Running...");
+
+            // Wait 8 seconds before putting the motors into idle-mode.
+            System.Threading.Thread.Sleep(8 * 1000);
+            motorPair.Idle();
+
+            Console.WriteLine("Done.");
+
+            // Disconnect from the NXT.
+            brick.Disconnect();
+
+            Console.ReadLine();
         }
     }
 }
