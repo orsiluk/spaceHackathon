5b2eebc8e812048ef246abcba49b49d1677097e9
Ross Gardiner <ross-public@gmx.com>
2018-02-28 13:12:44 +0000

    Initial commit of web server component

---
+++ server/RoverServer/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace RoverServer.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
