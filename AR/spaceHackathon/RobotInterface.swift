1aebb40d87318e3386e3366a414717f93f297c66
John Griffith <me@joh.ng>
2018-02-28 21:18:53 +0000

    Networking class

--- AR/spaceHackathon/RobotInterface.swift
+++ AR/spaceHackathon/RobotInterface.swift
@@ -7,7 +7,48 @@
 //
 
 import Foundation
+import Alamofire
 
+class RobotInterface {
+    
+    let queue = DispatchQueue(label: "com.cnoon.response-queue", qos: .utility, attributes: [.concurrent])
+    
+    let endpoint  = "http://localhost:50937/api/commands/Issue/"
+    
+    func moveForward(duration: Int32)  {
+        
+        let body : Parameters = [ "id " : "5", "CommandType": "Forward" , "Data": "payload test" ];
+        
+        Alamofire.request(self.endpoint, method: .post , parameters: body ).responseJSON { response in
+            
+            print("Request: \(String(describing: response.request))")   // original url request
+            print("Response: \(String(describing: response.response))") // http url response
+            print("Result: \(response.result)")                         // response serialization result
+            
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
+            
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                print("Data: \(utf8Text)") // original server data as UTF8 string
+            }
+            
+        }
+    }
+    
+    func threadTest()
+    {
+        DispatchQueue.global(qos: .userInitiated).async {
+            // Download file or perform expensive task
+            
+            DispatchQueue.main.async {
+                // Update the UI
+            }
+        }
+    }
+    
+    
+}
 
 
 
