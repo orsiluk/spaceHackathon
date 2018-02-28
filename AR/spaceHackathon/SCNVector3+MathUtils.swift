3e6667a6ad72b04c5e59ed6518852ad909dc4bbc
John Griffith <me@joh.ng>
2018-02-28 17:52:11 +0000

    Added maths util

---
+++ AR/spaceHackathon/SCNVector3+MathUtils.swift
@@ -0,0 +1,9 @@
+//
+//  SCNVector3+MathUtils.swift
+//  spaceHackathon
+//
+//  Created by John Griffith on 28/02/2018.
+//  Copyright © 2018 Orsolya Lukacs-Kisbandi. All rights reserved.
+//
+
+import Foundation
