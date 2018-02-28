08713322b3eb1d967dd3f570858f300a2ff46d79
Gavin Parker <gp14958@my.bristol.ac.uk>
2018-02-28 13:27:57 +0000

    Added mouse clicks

--- simulation/HackathonSim/Assets/Scripts/MouseController.cs
+++ simulation/HackathonSim/Assets/Scripts/MouseController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Unity;
 public class MouseController : MonoBehaviour {
 
 	// Use this for initialization
@@ -11,6 +11,19 @@ public class MouseController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetButtonDown("Fire1"))
+		{
+			mouseClick();
+		}
+	}
+
+
+
+	void mouseClick()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit))
+			GameObject.CreatePrimitive(PrimitiveType.Sphere).transform.position = hit.point;
 	}
 }
