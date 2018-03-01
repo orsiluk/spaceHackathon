e7427fd8cabd701575729a8fa95cc58275031d07
Orsi Lukacs <ol14474@my.bristol.ac.uk>
2018-03-01 18:51:16 +0000

    Finished AR - Has to be calibrated with the real rover

--- AR/spaceHackathon/ViewController.swift
+++ AR/spaceHackathon/ViewController.swift
@@ -16,13 +16,10 @@ enum GeometryType: Int{
     case plane = 2
 }
 
-
-
 class ViewController: UIViewController, ARSCNViewDelegate {
     
     @IBOutlet var sceneView: ARSCNView!
-    
-    
+
     var pointList: [SCNVector3] = []
     var loadcar: Bool = true
     var carObject = SCNNode()
@@ -55,76 +52,62 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             print("Done pressed")
             self.visualisePath()
             self.postPath()
-            
-//            self.postToServer(id: <#T##Float#>, commandType: <#T##String#>, distance: <#T##Float#>, angle: <#T##Float#>)
-//            let myUrl = URL(string:"http://2ba35d57.ngrok.io/api/Commands/Issue/")!
-////            let postString = "id=13&CommandType=Forward&Data=payload test"
-//
-//            let json: [String: Any] = ["id": "13",
-//                                       "CommandType": "Forward",
-//                                       "Data": "payload test" ]
-//
-//            let jsonData = try? JSONSerialization.data(withJSONObject: json)
-//
-////            request.httpBody = postString.data(using: .utf8)
-//
-//// ---------- Requests
-//            var request = URLRequest(url: myUrl)
-////            request.httpBody = postString.data(using: .utf8)
-//            request.httpBody = jsonData
-//            request.httpMethod = "POST"
-//            let (_, _, error) = URLSession.shared.synchronousDataTask(urlrequest: request)
-//            if let error = error {
-//                print("Synchronous task ended with error: \(error)")
-//            }
-//            else {
-//                print("Synchronous task ended without errors.")
-//            }
         }
-
     }
     
-    func postToServer(id:Int,commandType:String,value:Float){
-        let myUrl = URL(string:"http://openppr.eu.ngrok.io/api/Commands/Issue/")!
-        
-//        let command: [String: Any] = ["Id": String(id),
-//                                   "CommandType": commandType,
-//                                   "Data": String(value) ]
-//
-//        let jsonCommand = try? JSONSerialization.data(withJSONObject: command)
-        let parameters = ["Id": String(id), "CommandType": commandType, "Data": String(value)] as Dictionary<String, String>
-
-//        let data = "{\"Id\":\(id),\"CommandType\":\"\(commandType)\",\"Data\":\"\(value)\"}"
-        
-//        print(data)
-//        // Requests
-        var request = URLRequest(url: myUrl)
-//        let jsonCommand = try? JSONSerialization.data(withJSONObject: data, options: .prettyPrinted)
-        
-        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
-
-//        // request.httpBody = postString.data(using: .utf8)
-//        request.httpBody = jsonCommand
+    func makePostCall(id:Int,commandType:String,value:Float) {
+        let todosEndpoint: String = "http://openppr.eu.ngrok.io/api/Commands/Issue/"
+        guard let todosURL = URL(string: todosEndpoint) else {
+            print("Error: cannot create URL")
+            return
+        }
+        var todosUrlRequest = URLRequest(url: todosURL)
+        todosUrlRequest.httpMethod = "POST"
+        todosUrlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        todosUrlRequest.addValue("application/json", forHTTPHeaderField: "Accept")
+        let newTodo: [String: Any] = ["Id": id, "CommandType": commandType, "Data": Int(value)]
+        let jsonTodo: Data
         do {
-            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
+            jsonTodo = try JSONSerialization.data(withJSONObject: newTodo, options: [])
+            todosUrlRequest.httpBody = jsonTodo
+        } catch {
+            print("Error: cannot create JSON from todo")
+            return
+        }
+        
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: todosUrlRequest) {
+            (data, response, error) in
+            guard error == nil else {
+                print("error calling POST on /todos/1")
+                print(error!)
+                return
+            }
+            guard let responseData = data else {
+                print("Error: did not receive data")
+                return
+            }
             
-        } catch let error {
-            print(error.localizedDescription)
+            // parse the result as JSON, since that's what the API provides
+            do {
+                guard let receivedTodo = try JSONSerialization.jsonObject(with: responseData, options: []) as? [String: Any] else {
+                                                                            print("Could not get JSON from responseData as dictionary")
+                                                                            return
+                }
+                print("The todo is: " + receivedTodo.description)
+                
+                guard let todoID = receivedTodo["id"] as? Int else {
+                    print("Could not get todoID as int from JSON")
+                    return
+                }
+                print("The ID is: \(todoID)")
+            } catch  {
+                print("error parsing response from POST on /todos")
+                return
+            }
         }
-        
-        request.httpMethod = "POST"
-        
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        
-        let (_, _, error) = URLSession.shared.synchronousDataTask(urlrequest: request)
-        if let error = error {
-            print("Synchronous task ended with error: \(error)")
-        }
-        else {
-            
-            print("Synchronous task ended without errors.")
-        }
+        task.resume()
     }
     
     func postPath(){
@@ -138,13 +121,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             let angle = calculateAngle(pos1: pointList[index], pos2: pointList[index-1])
             let distance = getDistance(pos1: pointList[index], pos2: pointList[index-1])
             
-            let degrees = angle * 180/Float.pi
-            self.postToServer(id: counter, commandType: "Right", value: degrees)
-            counter = counter+1
-            self.postToServer(id: counter, commandType: "Forward", value: Float(distance*100))
-            counter = counter+1
+            var degrees = angle * 180/Float.pi
             
+            var dir =  "Right"
             
+            if degrees < 0{
+                dir = "Left"
+                degrees *= -1
+            }
+            
+            self.makePostCall(id: counter, commandType: dir, value: degrees)
+            counter = counter+1
+            self.makePostCall(id: counter, commandType: "Forward", value: Float(distance*100))
+            counter = counter+1
             
         }
     }
@@ -169,7 +158,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     @objc func tappedOnScreen(recognizer: UITapGestureRecognizer) {
         let sceneV = recognizer.view as! ARSCNView
         let touchLocation = recognizer.location(in: sceneV)
-
+        
         let hitResult = sceneV.hitTest(touchLocation, types: ARHitTestResult.ResultType.existingPlaneUsingExtent)
         if !hitResult.isEmpty {
             guard let hitResult = hitResult.first else {
@@ -177,7 +166,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             }
             self.addCircle(hitResult: hitResult)
         }
-        
     }
     
     func addCircle(hitResult: ARHitTestResult) {
@@ -192,12 +180,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             pointList.append(carObject.position)
             loadcar = false
         } else {
-            let box = SCNSphere(radius: 0.01)
+            let box = SCNSphere(radius: 0.03)
             let boxNode = SCNNode(geometry: box)
             
-            //        boxNode.physicsBody = SCNPhysicsBody(type: SCNPhysicsBodyType.dynamic, shape: SCNPhysicsShape(geometry: box, options: nil))
-            
-            //takes an enum
             boxNode.categoryBitMask = GeometryType.box.rawValue
             
             boxNode.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
@@ -206,10 +191,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             self.sceneView.scene.rootNode.addChildNode(boxNode)
             
             pointList.append(boxNode.position)
-
         }
-        
-        
     }
     
     func calculateAngle(pos1:SCNVector3,pos2:SCNVector3) -> (Float)  {
@@ -223,34 +205,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         var angle = atan2(1, 0) - atan2(pos2.z-pos1.z, pos2.x-pos1.x);
         
-        //angle = angle * 360 / (2*(Float.pi));
-
         if (angle < 0){
             angle = angle + 2 * (Float.pi);
         }
-
+        
+        if angle > (Float.pi){
+            angle = angle - 2*(Float.pi)
+        }
+        
         return angle
         
     }
-    
-    
-    
-//    func executePath(pointList: [SCNVector3]){
-//
-//        let pointCount = pointList.count
-//
-//        for index in 0...pointCount-1{
-//            //executeStage(pos1: pointList[index], pos2: pointList[index+1])
-//        }
-//    }
     
     func visualisePath(){
         
         let pointCount = pointList.count
         var animList: [SCNAction] = []
-        
-        //let carObject = addCar(x: pointList[0].x, y:pointList[0].y, z: pointList[0].z)
-        
         
         for index in 1...pointCount-1{
             
@@ -260,54 +230,49 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             let angle = calculateAngle(pos1: pointList[index], pos2: pointList[index-1])
             
             let and =  CGFloat(angle)
-            var yRot = CGFloat(carObject.rotation.y)
             
-            print("yrot")
-            print(yRot)
             print(pointList[index])
             let move = SCNAction.move(to: pointList[index], duration: 1.5)
             let rot = SCNAction.rotateTo(x: 0, y: and, z: 0, duration: 0.5)
             animList.append(rot)
             animList.append(move)
-            
-            
         }
         let animSequence = SCNAction.sequence(animList)
-
+        
         
         carObject.runAction(animSequence)
     }
-
+    
     func addLine(pos1:SCNVector3,pos2:SCNVector3) -> SCNNode {
         let lineLength = getDistance(pos1: pos1, pos2: pos2)
         let midPoint = findMidPoint(pos1: pos1, pos2: pos2)
-
+        
         let line = SCNCylinder(radius: 0.005, height: lineLength)
         let lineNode = SCNNode(geometry: line)
         
-
+        
         lineNode.categoryBitMask = GeometryType.box.rawValue
         lineNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
         lineNode.eulerAngles.x = -.pi / 2
         lineNode.position = midPoint
         
         return lineNode
-
+        
     }
     
     func getDistance(pos1:SCNVector3,pos2:SCNVector3) -> CGFloat{
-
+        
         print("Position1:",pos1)
         print("Position2:",pos2)
         let xLen = pos1.x - pos2.x
         let yLen = pos1.y - pos2.y
         let zLen = pos1.z - pos2.z
-
+        
         let len = sqrt(pow(xLen,2)+pow(yLen,2)+pow(zLen,2))
         print("------- Length : ",len)
         return CGFloat(len)
     }
-
+    
     func findMidPoint(pos1:SCNVector3,pos2:SCNVector3) -> SCNVector3{
         let xLen = (pos1.x + pos2.x)/2
         let yLen = (pos1.y + pos2.y)/2
@@ -317,7 +282,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     func createPlaneNode(anchor: ARPlaneAnchor) -> SCNNode {
         
-        let plane = SCNBox(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z), length: 0.02 , chamferRadius: 0)
+        let plane = SCNBox(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z), length: 0.002 , chamferRadius: 0)
         
         // SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))
         let lavaImage = UIImage(named: "mars")
@@ -330,7 +295,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let planeNode = SCNNode(geometry: plane)
         planeNode.position = SCNVector3Make(anchor.center.x, 0 , anchor.center.z)
         planeNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2, 1, 0, 0)
-        planeNode.opacity = 0.5
+        planeNode.opacity = 0.7
         return planeNode
     }
     
@@ -348,7 +313,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         carNode.position = SCNVector3(x, y, z)
         carNode.scale = SCNVector3(0.05, 0.05, 0.05)
-//        carNode.eulerAngles.y = -.pi / 2
+        //        carNode.eulerAngles.y = -.pi / 2
         return carNode
     }
     
@@ -367,8 +332,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         myObect.position = SCNVector3(x,y,z)
         return myObect
     }
-    
-    
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
@@ -410,55 +373,5 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
- 
-    }
-
-
-}
-
-//extension URLSession {
-//    func synchronousDataTask(with url: URL) -> (Data?, URLResponse?, Error?) {
-//        var data: Data?
-//        var response: URLResponse?
-//        var error: Error?
-//
-//        let semaphore = DispatchSemaphore(value: 0)
-//
-//        let dataTask = self.dataTask(with: url) {
-//            data = $0
-//            response = $1
-//            error = $2
-//
-//            semaphore.signal()
-//        }
-//        dataTask.resume()
-//
-//        _ = semaphore.wait(timeout: .distantFuture)
-//
-//        return (data, response, error)
-//    }
-//}
-
-extension URLSession {
-    func synchronousDataTask(urlrequest: URLRequest) -> (data: Data?, response: URLResponse?, error: Error?) {
-        var data: Data?
-        var response: URLResponse?
-        var error: Error?
-        
-        let semaphore = DispatchSemaphore(value: 0)
-        
-        let dataTask = self.dataTask(with: urlrequest) {
-            data = $0
-            response = $1
-            error = $2
-            
-            semaphore.signal()
-        }
-        dataTask.resume()
-        
-        _ = semaphore.wait(timeout: .distantFuture)
-        
-        return (data, response, error)
     }
 }
-
